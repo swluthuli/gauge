f5b72b46a5e8ae5c33eee41aa068cc88716fce42
swluthuli <swluthuli@gmail.com>
2019-09-15 13:43:18 +0200

    guage working with input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactSpeedometer from "react-d3-speedometer";
+import {InputGroup , Input} from 'reactstrap';
 
-function App() {
+class App extends React.Component {
+state={
+number:''
+}
+
+handleChange(value) {
+
+  this.setState({ number: value });
+}
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <InputGroup>
+      <Input type="number" placeholder ="number" name="number" value={this.state.number} onChange={e=>this.handleChange(e.target.value)} />
+      </InputGroup>
+
+      <div style={{ width: "500px", height: "300px",margin: "0 auto", paddingTop: "20px"}}>
+
+    <ReactSpeedometer
+        fluidWidth={true}
+        minValue={0}
+        maxValue={1000}
+        value={this.state.number}
+        needleColor="steelblue"
+        segments={4}
+        startColor="#EAAD09"
+        ringWidth={20}
+        currentValueText={this.state.number + "/ 1000 Points"}
+    />
+</div>
     </div>
   );
+}
 }
 
 export default App;
