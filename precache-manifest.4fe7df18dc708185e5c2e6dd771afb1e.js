58e3ac6e740305f0c0dd594e4d446d014cd4f58c
swluthuli <swluthuli@gmail.com>
2019-09-15 13:54:58 +0200

    Updates

---
+++ precache-manifest.4fe7df18dc708185e5c2e6dd771afb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20696a65ce7aa24c562ca7b7964b63e8",
+    "url": "/gauge/index.html"
+  },
+  {
+    "revision": "4e7dc8adb222e9ee27b8",
+    "url": "/gauge/static/css/main.4c2a360b.chunk.css"
+  },
+  {
+    "revision": "bc01a49ffb635db51fa0",
+    "url": "/gauge/static/js/2.a7e620d2.chunk.js"
+  },
+  {
+    "revision": "4e7dc8adb222e9ee27b8",
+    "url": "/gauge/static/js/main.0897df75.chunk.js"
+  },
+  {
+    "revision": "b44f57357e333d12945f",
+    "url": "/gauge/static/js/runtime~main.8436945a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gauge/static/media/logo.5d5d9eef.svg"
+  }
+]);
